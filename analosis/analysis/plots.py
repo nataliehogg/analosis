0a321f09f3795e1529478aedcacaeadd19e438a4
nataliehogg <mail@nataliebhogg.com>
2022-06-01 15:11:10 +0200

    interface script added. input vs ouput plot added

--- analosis/analysis/plots.py
+++ analosis/analysis/plots.py
@@ -1,11 +1,19 @@
 import numpy as np
 import pandas as pd
 import matplotlib
-from matplotlib import rc
+from matplotlib import rc, gridspec
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
 import copy
+import math
+import emcee
+from chainconsumer import ChainConsumer
+c = ChainConsumer()
+
+# colours for plots
+LOS         = ['#a6dba0','#5aae61','#1b7837']
+LOS_minimal = ['#c2a5cf', '#9970ab', '#762a83']
 
 # Use TeX
 rc('text', usetex=True)
@@ -21,16 +29,16 @@ class Plots:
 
         return plot
 
-    def image_plot(self, path, number_of_images, save=True, show=True):
+    def image_plot(self, path, number_of_images, number_of_columns=5, quality_cut=0, save=True, show=True):
+        print('Preparing image plot...')
+        if number_of_images > 10:
+            print('The plotter is slow but the result looks soooo good. Patience, my young padawan!')
 
         # Define the quality of images (the criterion is very empirical here)
         kwargs = pd.read_csv(str(path) + '/datasets/input_kwargs.csv')
         R_s = kwargs['R_sersic_sl'].to_numpy()
         beta = np.sqrt(kwargs['x_sl']**2. + kwargs['y_sl']**2.).to_numpy()
         quality = 1 / (1 + (beta/3/R_s)**2)
-        
-        # make this user-defined
-        quality_cut = 0
 
         filename = str(path) + '/datasets/image_list.pickle'
         infile = open(filename,'rb')
@@ -45,40 +53,137 @@ class Plots:
         v_min = -3
         v_max = 1
 
+        rows = int(math.ceil(number_of_images/number_of_columns))
 
-        if number_of_images == 1:
-            f, ax = plt.subplots(1, 1, figsize = (5,5), sharex=False, sharey=False)
-            im = ax.matshow(np.log10(image_list[0]), origin='lower', vmin=v_min, vmax=v_max, cmap=cmap, extent=[0, 1, 0, 1])
-            if quality < quality_cut:
-                ax.set_title('{:.2f}'.format(quality[0]), color='red', fontsize=12)
+        plt.rcParams['figure.constrained_layout.use'] = True
+
+        fig = plt.figure(figsize=(12,12))
+        gs = gridspec.GridSpec(rows, number_of_columns, figure=fig)
+        for n in range(number_of_images):
+            ax = fig.add_subplot(gs[n])
+            im = ax.matshow(np.log10(image_list[n]), origin='lower', vmin=v_min, vmax=v_max, cmap=cmap, extent=[0, 1, 0, 1])
+            if quality[n] < quality_cut:
+                ax.set_title('{:.2f}'.format(quality[n]), color='red', fontsize=8)
                 ax.plot([0,1],[0,1], color='red')
                 ax.plot([0,1],[1,0], color='red')
             else:
-                ax.set_title('{:.2f}'.format(quality[0]), fontsize=12)
+                ax.set_title('{:.2f}'.format(quality[n]), fontsize=8)
             ax.get_xaxis().set_visible(False)
             ax.get_yaxis().set_visible(False)
             ax.autoscale(False)
-        else:
-            if number_of_images >= 4:
-                f, ax = plt.subplots(int(np.sqrt(number_of_images)), int(np.sqrt(number_of_images)),
-                                 figsize=(10, 10), sharex=False, sharey=False)
-            else:
-                f, ax = plt.subplots(1, number_of_images, figsize = (10, 10), sharex=False, sharey=False)
 
-            for a, i in zip(ax.flat, range(number_of_images)):
-                im = a.matshow(np.log10(image_list[i]), origin='lower', vmin=v_min, vmax=v_max, cmap=cmap, extent=[0, 1, 0, 1])
-                if quality[i] < quality_cut:
-                    a.set_title('{:.2f}'.format(quality[i]), color='red', fontsize=12)
-                    a.plot([0,1],[0,1], color='red')
-                    a.plot([0,1],[1,0], color='red')
-                else:
-                    a.set_title('{:.2f}'.format(quality[i]), fontsize=12)
-                a.get_xaxis().set_visible(False)
-                a.get_yaxis().set_visible(False)
-                a.autoscale(False)
+        # fig.tight_layout()
 
         if save:
             plt.savefig(str(path) + '/plots/image.pdf', dpi=300, bbox_inches='tight')
+        if show:
+            plt.show()
+
+        return None
+
+    def input_output_plot(self, path, number_of_images, n_burn, quality_cut=0, show_not_converged=True, save=True, show=True):
+
+        in_kwargs = pd.read_csv(path + '/datasets/input_kwargs.csv')
+
+        # define the quality
+        R_s = in_kwargs['R_sersic_sl'].to_numpy()
+        beta = np.sqrt(in_kwargs['x_sl']**2. + in_kwargs['y_sl']**2.).to_numpy()
+        quality = 1 / (1 + (beta/3/R_s)**2)
+
+        in_gamma1 = in_kwargs['gamma1_los']
+        in_gamma2 = in_kwargs['gamma2_los']
+
+        # # flatten the lists
+        # in_gamma1 = [item for sublist in in_gammas1 for item in sublist]
+        # in_gamma2 = [item for sublist in in_gammas2 for item in sublist]
+
+        for i in range(number_of_images):
+            chain = path + '/chains/' + 'fit_image_' + str(i) + '.h5'
+            reader = emcee.backends.HDFBackend(filename = chain, name = 'lenstronomy_mcmc_emcee')
+            samples = reader.get_chain(discard = n_burn, flat = True)#, thin = 10)
+            c.add_chain(samples[:,2:4], walkers=np.shape(samples)[0], parameters = ['gamma1_los', 'gamma2_los'])
+        summary = c.analysis.get_summary()
+
+        # Remove the images under a certain quality
+        if quality_cut is not None:
+            summary = [s for i,s in enumerate(summary) if quality[i]>quality_cut]
+            in_gamma1 = [g for i, g in enumerate(in_gamma1) if quality[i]>quality_cut]
+            in_gamma2 = [g for i, g in enumerate(in_gamma2) if quality[i]>quality_cut]
+
+        # Isolate the cases where the MCMC did not converge
+        #summary_converged = [s for s in summary if s['gamma1_los'][0] is not None]
+        #summary_not_converged = [s for s in summary if s['gamma1_los'][0] is None]
+        summary_converged = []
+        indices_converged = []
+        summary_not_converged = []
+        indices_not_converged = []
+        for i in range(len(summary)):
+            s = summary[i]
+            if (s['gamma1_los'][0] is None
+                or s['gamma1_los'][2] is None
+                or s['gamma2_los'][0] is None
+                or s['gamma2_los'][2] is None):
+                indices_not_converged.append(i)
+                summary_not_converged.append(s)
+            else:
+                indices_converged.append(i)
+                summary_converged.append(s)
+
+        # plot the converged ones with error bars
+        in_gamma1_converged = [in_gamma1[i] for i in indices_converged]
+        in_gamma2_converged = [in_gamma2[i] for i in indices_converged]
+
+        out_gamma1 = [s['gamma1_los'][1] for s in summary_converged]
+        out_gamma2 = [s['gamma2_los'][1] for s in summary_converged]
+        gamma1_lower = [np.abs(s['gamma1_los'][0] - s['gamma1_los'][1]) for s in summary_converged]
+        gamma1_upper = [np.abs(s['gamma1_los'][2] - s['gamma1_los'][1]) for s in summary_converged]
+        gamma2_lower = [np.abs(s['gamma2_los'][0] - s['gamma2_los'][1]) for s in summary_converged]
+        gamma2_upper = [np.abs(s['gamma2_los'][2] - s['gamma2_los'][1]) for s in summary_converged]
+
+        fig, ax = plt.subplots(1, 1, figsize = (7,7), sharex=True, sharey=True)
+
+        plt.errorbar(in_gamma1_converged, out_gamma1, yerr = [gamma1_lower, gamma1_upper],
+                     ls = ' ', marker = '.', color = LOS[1], label = r'$\gamma_1^{\rm LOS}$')
+
+        plt.errorbar(in_gamma2_converged, out_gamma2, yerr = [gamma1_lower, gamma1_upper],
+                     ls = ' ', marker = '.', color = LOS_minimal[1], label = r'$\gamma_2^{\rm LOS}$')
+
+
+        if show_not_converged and len(indices_not_converged) > 0:
+            # plot the non-converged ones with crosses and without error bars
+            in_gamma1_not_converged = [in_gamma1[i] for i in indices_not_converged]
+            in_gamma2_not_converged = [in_gamma2[i] for i in indices_not_converged]
+
+            out_gamma1 = [s['gamma1_los'][1] for s in summary_not_converged]
+            out_gamma2 = [s['gamma2_los'][1] for s in summary_not_converged]
+
+            plt.plot(in_gamma1_not_converged, out_gamma1,
+                     ls = ' ', marker = 'x', markersize=10, color = LOS[1], label = r'non-converged chains')
+
+            plt.plot(in_gamma2_not_converged, out_gamma2,
+                     ls = ' ', marker = 'x', markersize=10, color = LOS_minimal[1])
+
+        ax.set_xlabel('Input $\gamma_{\\rm LOS}$')
+        ax.set_ylabel('Output $\gamma_{\\rm LOS}$')
+
+        # make an x = y line for the range of our plot
+        lims = [
+            np.min([ax.get_xlim(), ax.get_ylim()]),  # min of both axes
+            np.max([ax.get_xlim(), ax.get_ylim()]),  # max of both axes
+            ]
+
+        ax.plot(lims, lims, color = 'black', ls = '--', alpha=0.3, zorder=0)
+        ax.set_aspect('equal')
+        ax.set_xlim(lims)
+        ax.set_ylim(lims)
+
+        if quality_cut is not None:
+            plt.title(r"$Q > {}$".format(quality_cut))
+
+        plt.legend(frameon=False)
+
+        if save:
+            plt.savefig(str(path) + '/plots/input_output.pdf', dpi=300, bbox_inches='tight')
         if show:
             plt.show()
 
