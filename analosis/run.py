ea9a94bb89b0ffa62ebe12dce73e24a7cdffac3d
nataliehogg <mail@nataliebhogg.com>
2022-06-22 15:50:49 +0200

    roll back starting index feature

--- analosis/run.py
+++ analosis/run.py
@@ -5,12 +5,13 @@ import sys
 from pathlib import Path
 import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt
+
+# cosmology
 from colossus.cosmology import cosmology as colcos
 from colossus.lss import mass_function
-#import random as r
-#from lenstronomy.Cosmo.lens_cosmo import LensCosmo
 from astropy.cosmology import FlatLambdaCDM
+
+# lenstronomy
 from lenstronomy.ImSim.image_model import ImageModel
 import lenstronomy.Util.simulation_util as sim_util
 import lenstronomy.Util.image_util as image_util
@@ -19,12 +20,8 @@ from lenstronomy.Data.psf import PSF
 from lenstronomy.LensModel.lens_model import LensModel
 from lenstronomy.LightModel.light_model import LightModel
 from lenstronomy.Workflow.fitting_sequence import FittingSequence
-#from lenstronomy.LensModel.Profiles.sersic_utils import SersicUtil
-#import pickle
 
-#from analosis.image.composite_lens.composite_lens_generator import CompositeLens
-#from analosis.image.distributed_haloes.distributed_haloes_generator import DistributedHaloes
-
+# analosis
 from analosis.utilities.useful_functions import Utilities
 from analosis.image.mock_generator import Mocks
 from analosis.image.image_generator import Image
@@ -124,13 +121,13 @@ class Run:
                                                                          path)
 
         if settings['MCMC'] == True:
-            
+
             chain = MCMC(settings, baryons, halo, los, lens_light, Einstein_radii,
                          source, kwargs_data_list, kwargs_psf, kwargs_numerics, path)
-            
+
         elif settings['MCMC'] == False:
             print('MCMC will not be run.')
-            
+
         else:
             raise ValueError('MCMC must be True or False.')
 
